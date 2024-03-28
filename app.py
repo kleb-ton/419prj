548434626d3c4bc573361272384d01c5eaffe5c0
kleb-ton <caleb.tongst@gmail.com>
2024-03-27 21:40:58 -0700

    Implemented draft

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
- 
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template('homepage.html')
+    with open('e7_data/heronames.json', 'r') as file:
+        data = json.load(file)
+    return render_template('homepage.html', hero_list =data)
  
 if __name__ == '__main__':
     app.run()
