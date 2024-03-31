ac97f624e195c72e888d3ed51de80d3045e65885
kleb-ton <caleb.tongst@gmail.com>
2024-03-30 19:15:27 -0700

    Updated Css

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 import json
 import pandas as pd
 
@@ -9,6 +9,11 @@ def homepage():
     with open('e7_data/herocodes.json', 'r') as file:
         data = json.load(file)
     return render_template('homepage.html', hero_list =data)
+
+@app.route('/image')
+def get_vs():
+    filename = 'VS.png'  # Name of the image file
+    return send_file('images/' + filename)
  
 if __name__ == '__main__':
     app.run()
