334bfb215c4bcfd031ae22a437bc9496ccc4ad9a
Daniel Tham <dantham14@gmail.com>
2024-04-03 00:35:44 -0700

    radio button + draft into model

--- app.py
+++ app.py
@@ -19,11 +19,11 @@ def get_vs():
     filename = 'VS.png'  # Name of the image file
     return send_file('images/' + filename)
  
-@app.route('/calculateWin', methods=['POST'])
-def calculateWin():
+@app.route('/updateDraftPick', methods=['POST'])
+def updateDraftPick():
     data = request.json  # Read JSON data sent from client
     # Process the data here
-    print(data)
+    # print(data)
     draft_list = list(data.values())
         
     print(draft_list)
@@ -31,9 +31,21 @@ def calculateWin():
     res = draft_response(draft_list[0], draft_list[1], draft_list[2], draft_list[3], draft_list[4],
                    draft_list[5], draft_list[6], draft_list[7], draft_list[8], draft_list[9],
                    draft_list[10], draft_list[11], draft_list[12], draft_list[13])    
+
+    draft_response_vals = res 
     print(res)
-    # getModel()
-    return "Endpoint called successfully"
+    return render_template('homepage.html', draft_response_vals = draft_response_vals)
+
+@app.route('/calculateWin', methods=['POST'])
+def calculateWin():
+    data = request.json  # Read JSON data sent from client
+    # Process the data here
+    draft_list = list(data.values()) 
+    
+    print(draft_list)
+    pred = getModel(draft_list)
+    print(pred)
+    return render_template('homepage.html', pred = pred)
 
 if __name__ == '__main__':
     app.run()
--- model.py
+++ model.py
@@ -27,13 +27,18 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix
 
 
-def getModel():
+def getModel(draft):
     file_path = "e7_data/drafts_dataset.csv"
     df = pd.read_csv(file_path)
+    draft_df = pd.DataFrame(draft)
 
     y = df.iloc[:, -1]
     X = df.iloc[:, :-1]
     label_encoders = {}
+    
+    y_draft = draft_df.iloc[:, -1]
+    X_draft = draft_df.iloc[:, :-1]
+    draft_prediction_encoder = {}
 
     for column in X.select_dtypes(include=['object']).columns:
         label_encoders[column] = LabelEncoder()
@@ -41,7 +46,12 @@ def getModel():
         # To remove zero from the categories
         X[column] = label_encoders[column].fit_transform(X[column]) + 1
 
-    
+    # For input Draft for win prob
+    for column_draft in X_draft.select_dtypes(include=['object']).columns:
+        draft_prediction_encoder[column_draft] = LabelEncoder()
+
+        # To remove zero from the categories
+        X_draft[column_draft] = draft_prediction_encoder[column_draft].fit_transform(X_draft[column_draft]) + 1
 
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X)
@@ -49,14 +59,8 @@ def getModel():
     X_train, X_test, y_train, y_test = train_test_split(
         X_scaled, y, test_size=0.3, random_state=42)
 
-
-    
-
     model = LogisticRegression()
     model.fit(X_train, y_train)
-
-
-    
 
     y_pred = model.predict(X_test)
 
@@ -67,7 +71,10 @@ def getModel():
     print(f"Confusion Matrix:\n{conf_matrix}")
 
     X_sample = X_test[0].reshape(1, -1)
-
     pred = model.predict(X_sample)
+    
+    # model prediction 
+    X_draft_reshape = X_test[0].reshape(1, -1)
+    win_prob = model.predict(X_draft_reshape)
 
-    return model
+    return win_prob
