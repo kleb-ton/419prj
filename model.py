60e37df4b7e1b3691793a4b51157805b72b7abd5
kleb-ton <caleb.tongst@gmail.com>
2024-04-09 21:46:50 -0700

    Fixes

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import numpy as np
 # Models 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import svm
@@ -30,29 +30,26 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 def getModel(draft):
     file_path = "e7_data/drafts_dataset.csv"
     df = pd.read_csv(file_path)
-    draft_df = pd.DataFrame(draft)
+    draft.append("0")
+    colNames  =['enemy1',"main1",'enemy2', 'main2', 'enemy3','main3', 'main4', 'enemy4', 'main5','enemy5', 'main_pre_b1', 'enemy_pre_b1','main_pre_b2', 'enemy_pre_b2', 'main_post_b','enemy_post_b' ,'is_first']
+    draft_df = pd.DataFrame(np.array(draft).reshape(1,17),columns=colNames )
 
     y = df.iloc[:, -1]
     X = df.iloc[:, :-1]
     label_encoders = {}
-    
-    y_draft = draft_df.iloc[:, -1]
-    X_draft = draft_df.iloc[:, :-1]
-    draft_prediction_encoder = {}
 
+    print(X.iloc[12])
+    
+    X_draft = draft_df
+
+    X = pd.concat([X_draft, X], ignore_index=True)
     for column in X.select_dtypes(include=['object']).columns:
         label_encoders[column] = LabelEncoder()
-
+        X[column] = X[column].astype(str)
         # To remove zero from the categories
         X[column] = label_encoders[column].fit_transform(X[column]) + 1
-
-    # For input Draft for win prob
-    for column_draft in X_draft.select_dtypes(include=['object']).columns:
-        draft_prediction_encoder[column_draft] = LabelEncoder()
-
-        # To remove zero from the categories
-        X_draft[column_draft] = draft_prediction_encoder[column_draft].fit_transform(X_draft[column_draft]) + 1
-
+    first_row = X.iloc[0]
+    X = X[1:]
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X)
 
@@ -63,18 +60,18 @@ def getModel(draft):
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
+    print(y_pred)
 
     accuracy = accuracy_score(y_test, y_pred)
-    print(f"Accuracy: {accuracy}")
+    # print(f"Accuracy: {accuracy}")
 
     conf_matrix = confusion_matrix(y_test, y_pred)
-    print(f"Confusion Matrix:\n{conf_matrix}")
+    # print(f"Confusion Matrix:\n{conf_matrix}")
 
-    X_sample = X_test[0].reshape(1, -1)
-    pred = model.predict(X_sample)
+    X_sample = array(first_row).reshape(1, -1)
     
     # model prediction 
-    X_draft_reshape = X_test[0].reshape(1, -1)
-    win_prob = model.predict(X_draft_reshape)
-
+    win_prob = model.predict(X_sample)
+    np.set_printoptions(threshold=np.inf)
+    print(win_prob)
     return win_prob
